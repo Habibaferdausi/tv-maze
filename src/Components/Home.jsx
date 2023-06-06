edbf412151196c1b7a17fa02da5868abf52fad2f
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-06-06 14:49:09 +0600

    add home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import ShowList from "./Showlist";
+import ShowDetails from "./ShowDetails";
+import BookingForm from "./BookingForm";
 
 const Home = () => {
-  return <div></div>;
+  const [selectedShow, setSelectedShow] = useState(null);
+
+  const handleShowSelect = (show) => {
+    setSelectedShow(show);
+  };
+
+  return (
+    <div>
+      <h1>TV Shows App</h1>
+      {!selectedShow ? (
+        <ShowList onShowSelect={handleShowSelect} />
+      ) : (
+        <div>
+          <ShowDetails show={selectedShow} />
+          <BookingForm show={selectedShow} />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Home;
